2edf9c93b17f52820e0e47714fd2186f94a4a3ed
yorgis711 <105851696+yorgis711@users.noreply.github.com>
2023-02-04 12:59:39 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,3 +39,12 @@ export default function Home() {
     </Layout>
   );
 }
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page</p>
+    </Layout>
+  );
+}
